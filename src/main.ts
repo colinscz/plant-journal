dcd67c2af59ff9d23873a5768576cac116de406f
colinscz <19342760+colinscz@users.noreply.github.com>
2023-11-26 16:04:29 +0100

    feat: ignore type error

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createI18n } from 'vue-i18n';
 import { plugin, defaultConfig } from '@formkit/vue';
 import App from './App.vue';
 import router from './router';
+// @ts-ignore  
 import config from 'base/formkit.config';
 
 
